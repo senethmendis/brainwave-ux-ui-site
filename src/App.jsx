4c9424c121da048253c901bd1c2a68537f3995f9
wolfy-six <mendis.seneth@gmail.com>
2024-04-10 00:40:40 +0530

    app changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 const App = () => {
